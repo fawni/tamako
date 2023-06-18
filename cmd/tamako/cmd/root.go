c22289f413876e70f4d91e8c6abd716663a01c93
fawn <fawn@envs.net>
2023-06-18 18:35:23 +0300

    🚧 wip: cli progress (kinda)

--- cmd/tamako/cmd/root.go
+++ cmd/tamako/cmd/root.go
@@ -1,23 +1,32 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 
+	"github.com/fawni/tamako/api"
+	"github.com/parnurzeal/gorequest"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "tamako",
-	Short: "🐞 Cozy anonyomus whispers",
+var (
+	id    int64
+	limit int
 
-	CompletionOptions: cobra.CompletionOptions{
-		DisableDefaultCmd: true,
-	},
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {},
-}
+	rootCmd = &cobra.Command{
+		Use:   "tamako",
+		Short: "🐞 Cozy anonyomus whispers",
+
+		CompletionOptions: cobra.CompletionOptions{
+			DisableDefaultCmd: true,
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run(args)
+		},
+	}
+)
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -28,14 +37,59 @@ func Execute() {
 	}
 }
 
+func run(args []string) error {
+	req := gorequest.New()
+	var url string
+	if id != 0 {
+		url = fmt.Sprintf("http://localhost:8714/api/whisper/%d?pretty=true", id)
+	} else {
+		url = "http://localhost:8714/api/whisper?pretty=true"
+	}
+
+	if limit != 0 {
+		url = fmt.Sprintf("%s&limit=%d", url, limit)
+	}
+
+	_, body, errs := req.Get(url).End()
+	if errs != nil {
+		return errs[0]
+	}
+
+	if id != 0 || limit == 1 {
+		var whisper api.Whisper
+		if err := json.Unmarshal([]byte(body), &whisper); err != nil {
+			return err
+		}
+
+		var name string
+		if !(whisper.Name == "") {
+			name = whisper.Name
+		} else {
+			name = "anon"
+		}
+
+		fmt.Printf("%s: %s\n", name, whisper.Message)
+	} else {
+		var whispers []api.Whisper
+		if err := json.Unmarshal([]byte(body), &whispers); err != nil {
+			return err
+		}
+
+		for _, whisper := range whispers {
+			var name string
+			if !(whisper.Name == "") {
+				name = whisper.Name
+			} else {
+				name = "anon"
+			}
+			fmt.Printf("%s: %s\n", name, whisper.Message)
+		}
+	}
+
+	return nil
+}
+
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tamako.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().Int64VarP(&id, "id", "i", 0, "Whisper Snowflake ID")
+	rootCmd.Flags().IntVarP(&limit, "limit", "l", 0, "Limit the number of whispers to return")
 }
--- cmd/tamako/go.mod
+++ cmd/tamako/go.mod
@@ -2,9 +2,17 @@ module github.com/fawni/tamako
 
 go 1.20
 
-require github.com/spf13/cobra v1.7.0
+require (
+	github.com/parnurzeal/gorequest v0.2.16
+	github.com/spf13/cobra v1.7.0
+)
 
 require (
+	github.com/elazarl/goproxy v0.0.0-20221015165544-a0805db90819 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/smartystreets/goconvey v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	moul.io/http2curl v1.0.0 // indirect
 )
